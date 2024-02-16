ed66eac4817ebacd8cc1b963b3c711b12135bc08
NineVictory <160209417+NineVictory@users.noreply.github.com>
2024-02-16 16:01:27 +0900

    Add files via upload

---
+++ workspace/ch01_Java/workspace/ch01_Java/src/kr/s01/basic/HelloWorld.java
@@ -0,0 +1,16 @@
+package kr.s01.basic;
+//ctrl + F11 = 실행
+public class HelloWorld { //클래스시작
+	public static void main(String[] args) { //함수시작 , args=식별자
+							//S는 무조건 대문자
+		System.out.println("Hello World");
+				  //println = 함수(출력)
+	}//함수끝
+}//클래스 끝
+
+/* 
+ * 여러줄 주석
+*/
+/** 
+ * 문서 주석 ex) 설명란,도움말파일
+*/
